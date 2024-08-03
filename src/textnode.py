ae9df68bb702e8fdb661f863618d5afa8edbac10
nt2311-vn <nguyentoan231196@gmail.com>
2024-08-03 10:46:56 +0700

    setup gitignore for pycache and textnode class

---
+++ src/textnode.py
@@ -0,0 +1,17 @@
+class TextNode:
+    def __init__(self, text: str, text_type: str, url: str | None = None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, TextNode):
+            return False
+        return (
+            self.text == other.text
+            and self.text_type == other.text_type
+            and self.url == other.url
+        )
+
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
